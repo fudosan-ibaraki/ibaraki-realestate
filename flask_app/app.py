137340647252f124b335ccfbb34567b41303bf72
fudosan-ibaraki <mt.falcon2000@gmail.com>
2026-05-23 14:15:43 +0900

    サイトマップ追加

--- flask_app/app.py
+++ flask_app/app.py
@@ -755,8 +755,8 @@ def api_city_report():
         **pop_data,
     })
 
-
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host="0.0.0.0", port=port)
